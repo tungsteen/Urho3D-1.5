8f616f250c3c4f850184247e52e7aad11d1e8e2a
tungsteen <sergeybosko@googlemail.com>
2016-05-27 17:04:19 +0200

    add wave shader and wave scene

--- bin/FunWithShaders/Shaders/HLSL/PixelWave.hlsl
+++ bin/FunWithShaders/Shaders/HLSL/PixelWave.hlsl
@@ -6,25 +6,31 @@ void VS(float4 iPos : POSITION,
 		float2 iTexCoord : TEXCOORD0, 
 	out float2 oTexCoord : TEXCOORD0,
 	out float  oTime : TEXCOORD1,
-	out float  oColor : TEXCOORD2,
 	out float4 oPos : OUTPOSITION)
 {
     float4x3 modelMatrix = iModelMatrix;
 	float3 worldPos = GetWorldPos(modelMatrix);	
+    oPos = GetClipPos(worldPos);
 
-	oTexCoord = GetTexCoord(iTexCoord);
 	oTime = cElapsedTime;
-
-	worldPos.z += worldPos.z + 0.1 * sin((iPos.x + oTime) * 5.0);
-	
-	oPos = GetClipPos(worldPos);
+	oTexCoord = iTexCoord;
 }
 
 void PS(
 		float2 iTexCoord : TEXCOORD0,
 		float  iTime : TEXCOORD1,
-		float  iColor : TEXCOORD2,
     out float4 oColor : OUTCOLOR0)
 {
-    oColor = float4(0.0, 0.2, 0.3, 0.1);
+	float u;
+	float f = iTexCoord.x;
+	float offset = 0.4 + 0.4 * sin(iTime);
+    if (f > offset && f < offset + 0.2)
+    {
+      u = 1.0;
+    } 
+    else
+    {
+        u = 0.0;
+    }
+    oColor = float4(u, 1.0, 0.0, 1.0);
 }
