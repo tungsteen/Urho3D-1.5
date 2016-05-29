0028367eb1000a15f68c82af11252082feb51186
tungsteen <sergeybosko@googlemail.com>
2016-05-29 20:37:38 +0200

    add correct hlsl shaders

--- bin/FunWithShaders/Shaders/GLSL/Outline.glsl
+++ bin/FunWithShaders/Shaders/GLSL/Outline.glsl
@@ -4,31 +4,26 @@
 #include "ScreenPos.glsl"
 #include "Fog.glsl"
 
-varying vec2 vTexCoord;
 varying float vTime;
+
+uniform float cSiluetWidth;
+uniform vec4 cSiluetColor;
 
 void VS()
 {
   vTime = abs( cos( cElapsedTime ) );
-  vTexCoord = GetTexCoord( iTexCoord );
 
   mat4 modelMatrix = cModel;
+  vec3 worldNormal = GetWorldNormal( modelMatrix );
   vec3 worldPos = GetWorldPos( modelMatrix );
 
-  float scale = 0.02;
-  mat3 scaleMatrix = mat3x3(1.0 + scale, 0.0, 0.0, 0.0, 1.0 + scale, 0.0, 0.0, 0.0, 1.0 + scale);
-  vec3 scaleWorld = scaleMatrix * worldPos;
-  //scaleWorld.xy += vec2(1.0, 1.0);
-  gl_Position = GetClipPos( scaleWorld );
+  // shift towards normal multiplied by siluet width
+  worldPos.xy += worldNormal.xy * cSiluetWidth;
+
+  gl_Position = GetClipPos( worldPos );
 }
 
 void PS()
 {
-  vec4 diffInput = texture2D( sDiffMap, vTexCoord.xy );
-  vec4 diffColor = cMatDiffColor * diffInput;
-
-  // Per-pixel forward lighting
-  vec3 lightColor = cLightColor.rgb;
-
-  gl_FragColor = vec4( 191.0/255.0 * vTime, 193.0/255.0 * vTime, 194.0/255.0 * vTime, diffColor.a );
+  gl_FragColor = vec4( cSiluetColor.rgb * vTime, cSiluetColor.a );
 }
--- bin/FunWithShaders/Shaders/HLSL/Outline.hlsl
+++ bin/FunWithShaders/Shaders/HLSL/Outline.hlsl
@@ -2,29 +2,32 @@
 #include "Samplers.hlsl"
 #include "Transform.hlsl"
 
+uniform float cSiluetWidth;
+uniform float4 cSiluetColor;
+
 void VS(
-		float4 iPos : POSITION,
-		float2 iTexCoord : TEXCOORD0,
-	out	float  oTime : TEXCOORD1,
-	out float2 oTexCoord : TEXCOORD0,	
-	out float4 oPos : OUTPOSITION)
+    float4 iPos : POSITION,
+    float2 iTexCoord : TEXCOORD0,
+  out float  oTime : TEXCOORD1,
+  out float2 oTexCoord : TEXCOORD0,
+  out float4 oPos : OUTPOSITION)
 {
-    float4x3 modelMatrix = iModelMatrix;	
-	float3 worldPos = GetWorldPos(modelMatrix);	
-	
-	float scale = 0.01;
-	matrix<float, 3, 3> scaleMatrix = {1.0 + scale, 0.0, 0.0, 0.0, 1.0 + scale, 0.0, 0.0, 0.0, 1.0 + scale};
-	float3 scaleWorld = mul(scaleMatrix, worldPos);
-    oPos = GetClipPos(scaleWorld);
-	
-    oTime = abs(sin(cElapsedTime));
-	oTexCoord = iTexCoord;
+  float4x3 modelMatrix = iModelMatrix;
+  float3 worldPos = GetWorldPos(modelMatrix);
+  vec3 worldNormal = GetWorldNormal( modelMatrix );
+
+  // shift towards normal multiplied by siluet width
+  worldPos.xy += worldNormal.xy * cSiluetWidth;
+  oPos = GetClipPos(scaleWorld);
+
+  oTime = abs(sin(cElapsedTime));
+  oTexCoord = iTexCoord;
 }
 
 void PS(
-		float2 iTexCoord : TEXCOORD0,
+    float2 iTexCoord : TEXCOORD0,
         float  iTime : TEXCOORD1,
     out float4 oColor : OUTCOLOR0)
 {
-    oColor = float4(191.0/255.0 * iTime, 193.0/255.0 * iTime, 194.0/255.0 * iTime, 1.0);
+    oColor = float4(cSiluetColor.rgb * iTime, cSiluetColor.a);
 }
